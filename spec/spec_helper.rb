454de12f32de7ed4e6cf671a14339395877cc49a
Ralph Rooding <ralph@ralphrooding.com>
2014-05-01 11:04:29 +0200

    Comply with rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
-Dir[File.dirname(__FILE__) + "/steps/**/*_steps.rb"].each {|f| require f }
+Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
+Dir[File.dirname(__FILE__) + '/steps/**/*_steps.rb'].each { |f| require f }
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
